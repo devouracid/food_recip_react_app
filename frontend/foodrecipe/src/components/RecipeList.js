3ea7e3c1c2151fab211eb4fbc2774e952a4a2616
David Cuero <W5845542@student.hccs.edu>
2020-07-08 15:43:43 -0500

    Update RecipeList.js

---
+++ frontend/foodrecipe/src/components/RecipeList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class RecipeList extends Component {
+    render() {
+        return <React.Fragment>Hello From 
+            List</React.Fragment>;
+    }
+}
