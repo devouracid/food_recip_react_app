448273011fde58120e874d9dc0f8d4bc548f12c1
David Cuero <W5845542@student.hccs.edu>
2020-07-08 15:47:15 -0500

    Update RecipeSearch.js

---
+++ frontend/foodrecipe/src/components/RecipeSearch.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class RecipeSearch extends Component {
+    render() {
+        return <React.Fragment>Hello From 
+             Search</React.Fragment>;
+    }
+}
