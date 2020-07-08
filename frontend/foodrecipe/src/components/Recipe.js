22073a54695ba35545e388c33cf71f13e5546ce9
David Cuero <W5845542@student.hccs.edu>
2020-07-08 15:41:09 -0500

    Added, Recipe.Js,Details, List and Search in Components

---
+++ frontend/foodrecipe/src/components/Recipe.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class Recipe extends Component {
+    render() {
+        return <React.Fragment>Hello From 
+            Recipe</React.Fragment>;
+    }        
+        }
